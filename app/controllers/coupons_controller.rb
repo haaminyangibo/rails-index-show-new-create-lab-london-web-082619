6fcb077c3f4743775fea0e4a0ba680d3f5e595a1
Haami Nyangibo <haaminyangibo@hotmail.co.uk>
2019-09-21 12:48:28 +0100

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,26 @@
+class CouponsController < ApplicationController
+
+def index
+@coupons = Coupon.all
+end 
+
+def new 
+@coupon = Coupon.new
+end 
+
+def create
+@coupon = Coupon.new 
+@coupon.store = params[:coupon][:store]
+@coupon.coupon_code = params[:coupon][:coupon_code]
+
+@coupon.save
+
+redirect_to coupon_path(@coupon.id)
+end
+
+def show
+ @coupon = Coupon.find(params[:id])
+
+end 
+
+end
